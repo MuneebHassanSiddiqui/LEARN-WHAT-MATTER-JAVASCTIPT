13bd8fdca43a7ffa5d77a7ff2d667b52fde10cde
MuneebHassanSiddiqui <129599243+MuneebHassanSiddiqui@users.noreply.github.com>
2023-10-04 17:24:14 -0700

    Add files via upload

---
+++ NEXT-REACT.JS/app/page.js
@@ -0,0 +1,74 @@
+"use client"
+import { useState } from "react";
+
+
+const page = () => {
+const[ title,settitle] = useState("")
+const[ desc,setdesc] = useState("")
+const[ mainTask,setMainTask] = useState([])
+const submitHandler = (e) =>{
+e.preventDefault();
+setMainTask([...mainTask , { title , desc }]);
+settitle("");
+setdesc("");
+console.log(mainTask);
+
+};
+ const deleteHandler = (i) => {
+
+  let copytask = [...mainTask]
+  copytask.splice(i,1)
+  setMainTask(copytask)
+
+
+ }
+
+ let renderTask = <h2>No Task Available</h2> 
+
+ if(mainTask.length>0){
+  renderTask = mainTask.map((t,i)=>{
+    return <li key={i} className="flex items-center justify-between mb-5"><div className="flex justify-between mb-5 w-2/3">
+    <h5 className="text-2xl  font-semibold">{t.title}</h5>
+    <h6 className="text-lg  font-medium">{t.desc}</h6>
+    </div>
+    <button
+    onClick={()=>{
+      deleteHandler(i)
+    }} 
+    className="bg-red-400 text-white px-4 py-2 rounded font-bold">Delete</button>
+    </li>
+     })
+
+ }
+
+
+  return(
+  <>
+  <h1 className="bg-black text-white p-5 text-2xl font-bold text-center" >MUNIB'S TODO-LIST</h1>
+  <form onSubmit={submitHandler}>
+    <input type="text" className="text-2xl border-zinc-800 border-4 m-8 
+    px-4 py-2" placeholder="Enter Title here"
+      value={title}
+      onChange={(e)=>{
+settitle(e.target.value)
+      }}
+    />
+
+<input type="text" className="text-2xl border-zinc-800 border-4 m-8 
+    px-4 py-3" placeholder="Enter Description here"
+    value={desc}
+      onChange={(e)=>{
+setdesc(e.target.value)
+      }}
+    />
+     <button className="bg-black text-white px-4 py-2 text-2xl font-bold rounded m-5"> Add task</button>
+  </form>
+  <hr />
+  <div className="p-8"></div>
+  <ul className="text-center">{renderTask}
+  </ul>
+
+  </>
+  )
+}
+export default page
